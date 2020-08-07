17d88bdfd8ee300d1e40aba5c6a9ad839682bc10
sanil <sanilmanandhar@gmail.com>
2020-08-07 12:29:33 +0545

    message send to gmail

--- contact.php
+++ contact.php
@@ -1,16 +1,50 @@
 <?php
     require 'includes/header.php';
 ?>
-
 <?php
+    $result="";
+    require 'phpmailer/PHPMailer.php';
+    require 'phpmailer/SMTP.php';
+    require 'phpmailer/Exception.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
     if(isset($_POST['submit'])){
+        
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = "true";
+        $mail->SMTPSecure = "tls";
+        $mail->Port = "587";
+        $mail->Username = "blancmanandhar@gmail.com";
+        $mail->Password = "sabitashakyamanandhar";
+        $mail->Subject='Form Submission: ' . $_POST['subject'];
+        $mail->setFrom($_POST['email'],$_POST['name']);
+        $mail->isHTML(true);
+        $mail->Body='<p>Name: '. $_POST['name']. '<br>Email: ' .$_POST['email'].'<br>
+        Message: ' . $_POST['msg'] . '</p>';
+        $mail->addAddress('blancmanandhar@gmail.com');
+        
+        //$mail->addReplyTo($_POST['email'],$_POST['name']);        
 
+        if(!$mail->send()){
+            $result="Something went wrong. Please try again.";
+        }
+        else{
+            $result="Thanks " . $_POST['name']." for contacting us.";
+        }
+        $mail->smtpClose();
+        
     }
 ?>
 
 <h1>
     Please fill the form below
 </h1>
+<h5>
+    <?=$result; ?>
+</h5>
 
 <div class="contactForm">
     <form action='contact.php' method="POST">
