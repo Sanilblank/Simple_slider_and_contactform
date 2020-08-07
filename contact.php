a6aa99254be08ada19461bcb9c44eddfc57f7753
sanil <sanilmanandhar@gmail.com>
2020-08-07 10:32:24 +0545

    style contact form

--- contact.php
+++ contact.php
@@ -2,13 +2,16 @@
     require 'includes/header.php';
 ?>
 
-<form action=# method="POST">
-    <input type="text" name="name" placeholder="Enter your name" maxlength="30" minlength="4">
-    <input type="email" name="email" placeholder="Enter your email" maxlength="30" minlength="10">
-    <textarea name="message" rows="5" cols="20" placeholder="Write your message here"></textarea>
+<div class="contactForm">
+    <form action=# method="POST">
+        <input type="text" name="name" placeholder="Enter your name" maxlength="30" minlength="4">
+        <input type="email" name="email" placeholder="Enter your email" maxlength="30" minlength="10">
+        <input type="text" name="subject" placeholder="Enter your subject" maxlength="20" minlength="5">
+        <textarea name="msg" rows="5" cols="30" placeholder="Write your message here"></textarea>
 
-    <button type="submit" name="submit">Submit</button>
-</form>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</div>
 
 
 
