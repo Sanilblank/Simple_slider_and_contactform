509453f49d1c2d10157d2c2261b07dff55b92f09
sanil <sanilmanandhar@gmail.com>
2020-08-07 10:16:48 +0545

    text box in contact form

--- contact.php
+++ contact.php
@@ -2,6 +2,14 @@
     require 'includes/header.php';
 ?>
 
+<form action=# method="POST">
+    <input type="text" name="name" placeholder="Enter your name" maxlength="30" minlength="4">
+    <input type="email" name="email" placeholder="Enter your email" maxlength="30" minlength="10">
+    <textarea name="message" rows="5" cols="20" placeholder="Write your message here"></textarea>
+
+    <button type="submit" name="submit">Submit</button>
+</form>
+
 
 
 <?php
