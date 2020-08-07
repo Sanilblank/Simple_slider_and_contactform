eb0c71eee9773d370266695911bd592dcaa165aa
sanil <sanilmanandhar@gmail.com>
2020-08-07 10:41:22 +0545

    added folder phpmailer

--- contact.php
+++ contact.php
@@ -2,8 +2,18 @@
     require 'includes/header.php';
 ?>
 
+<?php
+    if(isset($_POST['submit'])){
+
+    }
+?>
+
+<h1>
+    Please fill the form below
+</h1>
+
 <div class="contactForm">
-    <form action=# method="POST">
+    <form action='contact.php' method="POST">
         <input type="text" name="name" placeholder="Enter your name" maxlength="30" minlength="4">
         <input type="email" name="email" placeholder="Enter your email" maxlength="30" minlength="10">
         <input type="text" name="subject" placeholder="Enter your subject" maxlength="20" minlength="5">
