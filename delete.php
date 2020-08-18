f6b8a85c810d0c572993a11411df82de7a0c5c97
sanil <sanilmanandhar@gmail.com>
2020-08-18 11:46:05 +0545

    delete complete

--- delete.php
+++ delete.php
@@ -1,7 +1,20 @@
 <?php
-    require 'includes/header.php';
-?>
+    require 'includes/database.php';
 
-<?php
-    require 'includes/footer.php';
+    if($_GET['delete']){
+        $id = $_GET['delete'];
+        $sql = "DELETE FROM contact WHERE id = ?";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            header('Location:messages.php?error=sqlerror');
+            exit;
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, 'i', $id);
+            mysqli_stmt_execute($stmt);
+
+            header('Location:messages.php?success=RecordDeleted');
+            exit();
+        }
+    }
 ?>
